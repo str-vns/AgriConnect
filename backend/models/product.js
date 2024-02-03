8df44d9494a9f90c1740c829ce011323dbc09588
Flappy <stvnsbarrantes@gmail.com>
2024-02-03 21:43:44 +0800

    model

---
+++ backend/models/product.js
@@ -0,0 +1,39 @@
+const { default: mongoose } = require("mongoose");
+
+const productModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter Product Name"],
+    trim: true,
+    maxlength: [150, "Product Name cant exceed to 100 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Enter A Discription of a Product"],
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productModel);
