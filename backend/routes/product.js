dc435cf5f28900fd6c17e624472489151adb35bc
Flappy <stvnsbarrantes@gmail.com>
2024-02-03 21:44:00 +0800

    prodyyyyy

---
+++ backend/routes/product.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../utility/multer')
+const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
+const { newProducts, updateProducts, getProduct, deleteProducts } = require('../controllers/productController');
+
+router.post('/addproduct', upload.single('avatar'),newProducts )
+router.get('/farmerproducts', getProduct)
+router.route('/updatedprod/:id').put( upload.single('avatar'), updateProducts).delete(deleteProducts)
+module.exports = router;
