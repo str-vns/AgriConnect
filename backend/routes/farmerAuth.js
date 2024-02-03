92b75e896a889fc79ad4a4b337566dc62042f25f
Flappy <stvnsbarrantes@gmail.com>
2024-02-03 21:43:54 +0800

    update farm route

--- backend/routes/farmerAuth.js
+++ backend/routes/farmerAuth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utility/multer');
 const { isAuthenticatedUser } = require('../middleware/auth');
-const { registerFarmer, getFarmer, getUpdateFarmer } = require('../controllers/farmerController');
+const { registerFarmer, getFarmer, getUpdateFarmer, deleteFarmer } = require('../controllers/farmerController');
 
 router.post('/register', upload.single('avatar'), registerFarmer)
 router.get('/allfarmer', getFarmer)
-router.route('/profile/:id').put( upload.single('avatar'), getUpdateFarmer)
+router.route('/profile/:id').put( upload.single('avatar'), getUpdateFarmer).delete(deleteFarmer)
 
 module.exports = router;
