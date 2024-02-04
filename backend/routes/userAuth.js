63eed20a3e653cac8ec1e8908edb1b38f7afabf7
Flappy <stvnsbarrantes@gmail.com>
2024-02-04 20:33:24 +0800

    update user route

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../utility/multer');
-const {registerUser, getUser, getUpdateUser, deleteUser} = require('../controllers/userController')
+const {registerUser, getUser, getUpdateUser, deleteUser, UserLogin, UserLogout} = require('../controllers/userController')
 const { isAuthenticatedUser } = require('../middleware/auth');
 
 router.post('/register', upload.single('avatar'), registerUser)
 router.get('/alluser', getUser)
 router.route('/profile/:id').put( getUpdateUser).delete( deleteUser)
+router.post('/login',  UserLogin)
+router.get('/logout', UserLogout)
 
 module.exports = router;
