449e0f3861b97b0d41e8da26089d5d3b546834d4
Flappy <stvnsbarrantes@gmail.com>
2024-02-04 20:44:11 +0800

    bkcontrol update js

--- backend/controllers/bankController.js
+++ backend/controllers/bankController.js
@@ -6,37 +6,37 @@ const Bank = require('../models/bank')
 //create
 exports.newBank = async (req, res, next) => {
 
-    // let images = []
-    //   if (typeof req.body.images === 'string') {
-    //       images.push(req.body.images)
-    //   } else {
-    //       images = req.body.images
-    //   }
+    let images = []
+      if (typeof req.body.images === 'string') {
+          images.push(req.body.images)
+      } else {
+          images = req.body.images
+      }
   
-    //   let imagesLinks = [];
+      let imagesLinks = [];
   
-    //   for (let i = 0; i < images.length; i++) {
-    //       let imageDataUri = images[i]
-    //       try {
-              const result = await cloudinary.v2.uploader.upload(  req.body.images, {
+      for (let i = 0; i < images.length; i++) {
+          let imageDataUri = images[i]
+          try {
+              const result = await cloudinary.v2.uploader.upload(`${imageDataUri}`, {
                   folder: 'banks',
                   width: 150,
                   crop: "scale",
               });
   
-    //           imagesLinks.push({
-    //               public_id: result.public_id,
-    //               url: result.secure_url
-    //           })
+              imagesLinks.push({
+                  public_id: result.public_id,
+                  url: result.secure_url
+              })
   
-    //       } catch (error) {
-    //           console.log(error)
-    //       }
+          } catch (error) {
+              console.log(error)
+          }
   
-    //   }
+      }
   
-    //   req.body.images = imagesLinks
-    //   req.body.user = req.user.id;
+      req.body.images = imagesLinks
+      req.body.user = req.user.id;
     const { bankName, city, postalCode, address, location, coordinates } = req.body;
       const bank = await Bank.create({
         bankName,
