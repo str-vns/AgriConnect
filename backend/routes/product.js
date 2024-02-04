e16d677a072ff3db7b552e6f87efd4d9e3fa3a39
Flappy <stvnsbarrantes@gmail.com>
2024-02-04 20:40:14 +0800

    product route update

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,7 +4,7 @@ const upload = require('../utility/multer')
 const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
 const { newProducts, updateProducts, getProduct, deleteProducts } = require('../controllers/productController');
 
-router.post('/addproduct', upload.single('images'),newProducts )
+router.post('/addproduct', upload.array('images', 10), newProducts )
 router.get('/farmerproducts', getProduct)
-router.route('/updatedprod/:id').put( upload.single('images'), updateProducts).delete(deleteProducts)
+router.route('/updatedprod/:id').put( upload.array('images', 10), updateProducts).delete(deleteProducts)
 module.exports = router;
