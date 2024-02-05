6e64b6ff51c7c7dd66823296a3c42e6613a9b6b1
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-02-05 15:54:12 +0800

    Home

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,18 +1,10 @@
 import React, { Fragment, useEffect, useState } from "react";
-import Loader from "./Layout/Loader";
 import { useParams } from "react-router-dom";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 import axios from "axios";
-import MetaData from "./Layout/MetaData";
-import ReactPaginate from "react-paginate";
-import Product from "./Product/Product";
-
 const Home = () => {
 
   return (
     <div className="bg-white">
-     
     </div>
   );
 };
