f5f8431a3527d8cb3e3b93ec6c7b4706296cb8a7
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-02-06 15:32:52 +0800

    home

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Header from "./Layout/Header";
 const Home = () => {
 
   return (
     <div className="bg-white">
+      <Header />
     </div>
   );
 };
