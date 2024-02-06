07cd3b3b7aaae3794cf1a1b4396e5e33095f34ed
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-02-06 15:32:09 +0800

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './Components/Home';
 import Header from './Components/Layout/Header'
+import Login from './Components/User/Login';
+import Register from './Components/User/Register';
+
 function App() {
  
 
   return (
          <Router>
-           <Header />
+      
           <Routes>
             <Route path="/" element = {<Home />}/>
-
-
+            <Route path="/login" element={<Login />}  exact="true" />
+            <Route path="/register" element={<Register />} />
           </Routes>
 
          </Router>
