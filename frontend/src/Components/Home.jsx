82784231f021b4d94ec60a4b15765901039b0c7d
Flappy <stvnsbarrantes@gmail.com>
2024-02-09 21:45:32 +0800

    HOMie UPdate

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,12 +2,20 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "./Layout/Header";
+import Mappy from "./Mapps/MappY"
 const Home = () => {
 
   return (
-    <div className="bg-white">
-      <Header />
-    </div>
+    
+    <div className="flex">
+
+    <Header />
+
+  <div className="w-full ">
+   <Mappy />
+  </div>
+ 
+</div>
   );
 };
 
