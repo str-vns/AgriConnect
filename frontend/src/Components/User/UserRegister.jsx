439a3b7a12281969b0882ca43ebbd370d66b0ec4
Flappy <stvnsbarrantes@gmail.com>
2024-02-09 21:45:40 +0800

    Register Shees

--- frontend/src/Components/User/UserRegister.jsx
+++ frontend/src/Components/User/UserRegister.jsx
@@ -15,9 +15,9 @@ const UserRegister = () => {
       <Header />
     </div>
 
-<section class="bg-white lg:w-5/6">
+<section class="w-full md:w-5/6 mr-0 md:mr-4 xl:mr-60">
   <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
-    <section class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+    <section class="relative flex h-32  items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
       <img
         alt="Night"
         src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
