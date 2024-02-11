7ec768b4bd0dcf201b460670de94901d4341da05
Flappy <stvnsbarrantes@gmail.com>
2024-02-12 00:49:19 +0800

    App Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './Components/Home';
@@ -9,22 +9,37 @@ import FarmRegister from './Components/User/FarmRegister';
 import UserRegister from './Components/User/UserRegister';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import FarmerLocation from './Components/User/FarmerLocation';
+import { useState } from 'react';
+import Test from './Test';
+
 function App() {
- 
+  const [state, setState] = useState({
+    farmCollection: localStorage.getItem('farmCollection')
+      ? JSON.parse(localStorage.getItem('farmCollection'))
+      : {}
+  });
 
+  const saveFarmerRegister = async (data) => {
+    console.log(data)
+    setState({ ...state, farmCollection: data });
+    localStorage.setItem('farmCollection', JSON.stringify(data));
+  }
+  console.log(state.farmCollection);
   return (
-         <Router>
+    <Router>
       <ToastContainer />
-          <Routes>
-            <Route path="/" element = {<Home />}/>
-            <Route path="/login" element={<Login />}  exact="true" />
-            <Route path="/register" element={<Register />} />
-            <Route path="/userRegister" element={<UserRegister/>}/>
-            <Route path="/farmerRegister" element={<FarmRegister/>}/>
-          </Routes>
-
-         </Router>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} exact="true"/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/userRegister" element={<UserRegister />} />
+        <Route path="/farmerRegister" element={<FarmRegister farmerInfo={state.farmCollection} saveFarmerRegister={saveFarmerRegister} />} />
+        <Route path="/farmerLocation" element={<FarmerLocation farmCollection={state.farmCollection} />} />
+        <Route path="/test" element={<Test/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
