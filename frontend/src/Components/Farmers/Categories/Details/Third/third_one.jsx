3f3a7119d3c695f0813da3d60caeda0869cffa10
princessjoyy <150142779+princessjoyy@users.noreply.github.com>
2024-02-16 02:08:49 +0800

    first_program

--- frontend/src/Components/Farmers/Categories/Details/Third/third_one.jsx
+++ frontend/src/Components/Farmers/Categories/Details/Third/third_one.jsx
@@ -1,77 +1,74 @@
 import React from 'react';
+
 const THIRDONE = () => (
-<>
-  <meta charSet="utf-8" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-  />
-  {/* OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn */}
-  <meta property="og:site_name" content="" /> {/* website name */}
-  <meta property="og:site" content="" /> {/* website link */}
-  <meta property="og:title" content="" />{" "}
-  {/* title shown in the actual shared post */}
-  <meta property="og:description" content="" />{" "}
-  {/* description shown in the actual shared post */}
-  <meta property="og:image" content="" /> {/* image link, make sure it's jpg */}
-  <meta property="og:url" content="" />{" "}
-  {/* where do you want your post to link to */}
-  <meta name="twitter:card" content="summary_large_image" />{" "}
-  {/* to have large image post format in Twitter */}
-  {/* Webpage Title */}
-  <title>Details</title>
-  {/* Styles */}
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
-    rel="stylesheet"
-  />
-  <link href="farmers/css/fontawesome-all.css" rel="stylesheet" />
-  <link
-    href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-    rel="stylesheet"
-  />
-  <link href="farmers/css/swiper.css" rel="stylesheet" />
-  <link href="farmers/css/magnific-popup.css" rel="stylesheet" />
-  <link href="farmers/css/styles.css" rel="stylesheet" />
-  {/* Favicon  */}
-  <link rel="icon" href="farmers/images/favicon.png" />
-  {/* Navigation */}
-  <nav className="navbar fixed-top">
-    <div className="container flex flex-wrap items-center justify-between sm:px-4 lg:flex-nowrap lg:px-8">
-      <a
-        className="inline-block mr-4 py-0.5 text-xl whitespace-nowrap hover:no-underline focus:no-underline"
-        href="index.html"
-      >
-      </a>
-      <button
-        className="background-transparent rounded text-xl leading-none hover:no-underline focus:no-underline lg:hidden lg:text-gray-400"
-        type="button"
-        data-toggle="offcanvas"
-      >
-        <span className="navbar-toggler-icon inline-block w-8 h-8 align-middle" />
-      </button>
-      <div
-        className="navbar-collapse offcanvas-collapse lg:flex lg:flex-grow lg:items-center"
-        id="navbarsExampleDefault"
-      >
+  <>
+    <meta charSet="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    {/* OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn */}
+    <meta property="og:site_name" content="" /> {/* website name */}
+    <meta property="og:site" content="" /> {/* website link */}
+    <meta property="og:title" content="" />{" "}
+    {/* title shown in the actual shared post */}
+    <meta property="og:description" content="" />{" "}
+    {/* description shown in the actual shared post */}
+    <meta property="og:image" content="" /> {/* image link, make sure it's jpg */}
+    <meta property="og:url" content="" />{" "}
+    {/* where do you want your post to link to */}
+    <meta name="twitter:card" content="summary_large_image" />{" "}
+    {/* to have large image post format in Twitter */}
+    {/* Webpage Title */}
+    <title>Details</title>
+    {/* Styles */}
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+      rel="stylesheet"
+    />
+    <link href="farmers/css/fontawesome-all.css" rel="stylesheet" />
+    <link
+      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <link href="farmers/css/swiper.css" rel="stylesheet" />
+    <link href="farmers/css/magnific-popup.css" rel="stylesheet" />
+    <link href="farmers/css/styles.css" rel="stylesheet" />
+    {/* Favicon  */}
+    <link rel="icon" href="farmers/images/favicon.png" />
+    {/* Navigation */}
+    <nav className="navbar fixed-top">
+      <div className="container flex flex-wrap items-center justify-between sm:px-4 lg:flex-nowrap lg:px-8">
+        <a
+          className="inline-block mr-4 py-0.5 text-xl whitespace-nowrap hover:no-underline focus:no-underline"
+          href="index.html"
+        >
+        </a>
+        <button
+          className="background-transparent rounded text-xl leading-none hover:no-underline focus:no-underline lg:hidden lg:text-gray-400"
+          type="button"
+          data-toggle="offcanvas"
+        >
+          <span className="navbar-toggler-icon inline-block w-8 h-8 align-middle" />
+        </button>
+        <div
+          className="navbar-collapse offcanvas-collapse lg:flex lg:flex-grow lg:items-center"
+          id="navbarsExampleDefault"
+        >
+        </div>{" "}
+        {/* end of navbar-collapse */}
       </div>{" "}
-      {/* end of navbar-collapse */}
-    </div>{" "}
-    {/* end of container */}
-  </nav>{" "}
-  {/* end of navbar */}
-  {/* end of navigation */}
-  {/* Header */}
-  <header className="ex-header bg-gray">
-  <div className="flex justify-center items-center">
-  <h1 className="xl:ml-24">Article Details</h1>
-</div>
-    {/* end of container */}
-  </header>{" "}
-  {/* end of ex-header */}
-  {/* end of header */}
-  {/* Basic */}
+      {/* end of container */}
+    </nav>{" "}
+    {/* end of navbar */}
+    {/* end of navigation */}
+    {/* Header */}
+    <header className="ex-header bg-gray text-center py-12">
+      <h1 className="xl:ml-24 text-6xl">Agricultural Competitiveness Enhancement Fund (ACEF)</h1>
+    </header>{" "}
+    {/* end of ex-header */}
+    {/* Basic */}
   <div className="ex-basic-1 py-12">
     <div className="container px-4 sm:px-8">
       <img
@@ -85,219 +82,75 @@ const THIRDONE = () => (
   {/* end of ex-basic-1 */}
   {/* end of basic */}
   {/* Basic */}
-  <div className="ex-basic-1 pt-4">
-    <div className="container px-4 sm:px-8 xl:px-32">
-      <p className="mb-4">
-        {" "}
-        Are you looking for ways to grow the user base for your mobile
-        application? Then you have arrived at the right place. Here you will
-        find a curated collection of landing page HTML templates that will help
-        you build an engaging online presentation for your mobile app and
-        convince visitors to become loyal paying users.
-      </p>
-      <p className="mb-12">
-        {" "}
-        All templates in the roundup are premium which means you need to pay for
-        them but we're talking small amounts of money which won't break your
-        bank account but will help authors make a living. In return you get a
-        high quality, updated item together with high quality and very prompt
-        technical support.
-      </p>
-      <h2 className="mb-4">Advantages of working with this template</h2>
-      <p className="mb-4">
-        Besides buying the template you need some basic web skills in order to
-        customize it. Nothing too fancy HTML/CSS will do just fine and a little
-        bit of image editing. You can always hire a web designer to help with
-        the customization work while you provide the template and the content
-        that is the most important.
-      </p>
-      <p className="mb-6">
-        Riga is a landing page HTML template made with Tailwind CSS to help you
-        showcase your mobile app online and persuade visitors to download it
-        from the app stores. The author used Tailwind CSS to build the template
-        and integrated a nice animated navigation that slides from outside the
-        screen.
-      </p>
-    </div>{" "}
-    {/* end of container */}
-  </div>{" "}
-  {/* end of ex-basic-1 */}
-  {/* end of basic */}
-  {/* Cards */}
-  <div className="ex-cards-1 py-4">
+    {/* end of header */}
+    {/* Content */}
     <div className="container px-4 sm:px-8">
-      {/* Card */}
-      <div className="card">
-        <ul className="list-unstyled">
-          <li className="flex">
-            <span className="fa-stack">
-              <span className="fas fa-circle fa-stack-2x" />
-              <span className="fa-stack-1x">1</span>
-            </span>
-            <div className="flex-1 ml-3">
-              <h5 className="mt-0.5 mb-1.5">High Quality Service</h5>
-              <p className="mb-4">
-                Features include an eye catching morphtext in the header,
-                details lightbox for more details information
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>{" "}
-      {/* end of card */}
-      {/* end of card */}
-      {/* Card */}
-      <div className="card">
-        <ul className="list-unstyled">
-          <li className="flex">
-            <span className="fa-stack">
-              <span className="fas fa-circle fa-stack-2x" />
-              <span className="fa-stack-1x">2</span>
-            </span>
-            <div className="flex-1 ml-3">
-              <h5 className="mt-0.5 mb-1.5">Prompt Timely Delivery</h5>
-              <p className="mb-4">
-                Statistics numbers for important values, card slider for
-                testimonials, image slider for customer logos
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>{" "}
-      {/* end of card */}
-      {/* end of card */}
-      {/* Card */}
-      <div className="card">
-        <ul className="list-unstyled">
-          <li className="flex">
-            <span className="fa-stack">
-              <span className="fas fa-circle fa-stack-2x" />
-              <span className="fa-stack-1x">3</span>
-            </span>
-            <div className="flex-1 ml-3">
-              <h5 className="mt-0.5 mb-1.5">Skilled Team Involved</h5>
-              <p className="mb-4">
-                Some useful extra pages are bundled with the template lik
-                article details, terms conditions and privacy policy
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>{" "}
-      {/* end of card */}
-      {/* end of card */}
-    </div>{" "}
-    {/* end of container */}
-  </div>{" "}
-  {/* end of ex-cards-1 */}
-  {/* end of cards */}
-  {/* Basic */}
-  <div className="ex-basic-1 pt-6 pb-12">
-    <div className="container px-4 sm:px-8 xl:px-32">
-      <p className="mb-12">
-        {" "}
-        Features include an eye catching morphtext in the header, details
-        lightbox for more details information, statistics numbers for important
-        values, card slider for testimonials, image slider for customer logos
-        and working forms that will enable your landing page to provide leads in
-        order to achieve your marketing goals.
-      </p>
-      <h2 className="mb-6">New elements added to the package</h2>
-      <img
-        className="inline mb-12"
-        src="images/article-details-small.jpg"
-        alt="alternative"
-      />
-      <p className="mb-4">
-        {" "}
-        Some useful extra pages are bundled with the template lik article
-        details, terms conditions and privacy policy which can be customized as
-        per your requirements. Riga has an impactful dark style design combined
-        with warm colors and angular background shapes with the unique goal of
-        making your mobile app stand out.{" "}
-      </p>
-      <p className="mb-12">
-        {" "}
-        Leon is an easy to customize landing page HTML template built with
-        Tailwind CSS for promoting mobile apps to the online audience and making
-        visitors download them from app stores. This template is built with HTML
-        and Tailwind CSS to ensure the highest flexibility for all users.
-      </p>
-      <div className="text-box mb-12">
-        <h3 className="mb-2">
-          Visitors love a beautiful and efficient website
-        </h3>
-        <p className="mb-4">
-          {" "}
-          All designers, developers and tech-savvy people will be able to
-          customize this template with basic web coding skills. Among the
-          features you will find details lightbox for more details information,
-          tabbed content for feature details, video lightbox, card slider for
-          testimonials, statistics numbers, image slider for customer logos,
-          dropdown navigation and useful extra pages for article details, terms.
+      <section className="py-1">
+        <h2 className="text-3xl font-bold mb-4">The Agricultural Competitiveness Enhancement Fund (ACEF)</h2>
+        <p className="mb-4 text-xl">
+          The primary objective of the ACEF Lending Program is to offer essential credit to farmers, fisherfolk, their cooperatives and associations, as well as micro and small enterprises, with the aim of boosting their productivity. Additionally, the program seeks to establish an agricultural lending initiative that improves the competitiveness of the intended beneficiaries or sectors, particularly small-scale farmers and fisherfolk.
         </p>
-      </div>{" "}
-      {/* end of text-box */}
-      <p className="mb-6">
-        {" "}
-        Together with touches of call to action green, the overall landing page
-        design will showcase your app beautifully and will help with convincing
-        visitors to download it from the app stores. Orange and gray hues with
-        one color backgrounds to maximize the impact of your content image
-        slider for customer logos and working forms.
-      </p>
-      <ul className="list-unstyled mb-12 space-y-2">
-        <li className="flex">
-          <i className="fas fa-square" />
-          <div className="flex-1 ml-2">
-            <strong>Card slider</strong> for testimonials, statistics numbers
-            for important values image slider for logos
-          </div>
-        </li>
-        <li className="flex">
-          <i className="fas fa-square" />
-          <div className="flex-1 ml-2">
-            <strong>Dropdown navigation</strong> and useful extra pages for
-            article details, terms conditions and privacy policy
-          </div>
-        </li>
-        <li className="flex">
-          <i className="fas fa-square" />
-          <div className="flex-1 ml-2">
-            <strong>Clean light design</strong> mixing purple, orange and gray
-            hues with one color backgrounds to maximize
-          </div>
-        </li>
-        <li className="flex">
-          <i className="fas fa-square" />
-          <div className="flex-1 ml-2">
-            <strong>Together with</strong> touches of call to action green, the
-            overall landing page design will showcase your app
-          </div>
-        </li>
-        <li className="flex">
-          <i className="fas fa-square" />
-          <div className="flex-1 ml-2">
-            <strong>This template</strong> is built with HTML and Tailwind CSS
-            to ensure the highest flexibility for all users
-          </div>
-        </li>
-      </ul>
+        <h3 className="text-2xl font-bold mb-2">Primary Objectives:</h3>
+        <ul className="list-disc ml-8 mb-4 text-xl">
+          <li>Allocate essential financial resources to enhance productivity.</li>
+          <li>Create an agricultural lending program that improves competitiveness.</li>
+        </ul>
+      </section>
+      <section className="py-1">
+        <h2 className="text-2xl font-bold mb-4">Who can avail?</h2>
+        <ul className="list-disc ml-8 mb-4 text-xl">
+          <li>Independent agriculturalists and fishermen</li>
+          <li>Micro and small enterprises</li>
+          <li>Agricultural and aquatic industry cooperatives and associations</li>
+        </ul>
+      </section>
+
+      <section className="py-1">
+        <h2 className="text-2xl font-bold mb-4">Permissible projects/loan purposes</h2>
+        <ul className="list-disc ml-8 mb-4 text-xl">
+          <li>Acquisition of agricultural supplies, machinery, or infrastructure upgrades for the farm</li>
+          <li>Procurement or establishment of agricultural machinery, equipment, and facilities for production, post-production, and processing purposes.</li>
+        </ul>
+      </section>
+
+      <section className="py-1">
+        <h2 className="text-2xl font-bold mb-4">Maximum loanable amount</h2>
+        <p className="mb-4 text-xl">
+          The loanable amount is a maximum of 90% of the total project cost, with the following limitations:
+        </p>
+        <ul className="list-disc ml-8 mb-4 text-xl">
+          <li>The amount of financial assistance provided is ₱1 Million for each individual farmer or fisherfolk, and ₱5 Million for each project loan granted to farmers and fisherfolk cooperatives, associations, and micro and small enterprises (MSE).</li>
+        </ul>
+      </section>
+
+      <section className="py-1">
+        <h2 className="text-2xl font-bold mb-4">Rate of interest</h2>
+        <p className="mb-4 text-xl">
+          2% annually
+        </p>
+        <h2 className="text-2xl font-bold mb-4">Security</h2>
+        <ul className="list-disc ml-8 mb-4 text-xl">
+          <li>Collateral: Production</li>
+          <li>Combination of the following: Assignment of insurance proceeds, if applicable; and Assignment of expected produce</li>
+          <li>Acquisition of fixed asset</li>
+          <li>Any or combination of the following: Chattel mortgage on object of financing Assignment of expected produce PCIC Insurance proceeds</li>
+        </ul>
+      </section>
+
       <a className="btn-solid-reg mb-12" href="/third">
         Back
-      </a>
-    </div>{" "}
+        </a>
+
+    </div>
     {/* end of container */}
-  </div>{" "}
-  {/* end of ex-basic-1 */}
-  {/* end of basic */}
-  {/* Scripts */}
-  {/* jQuery for JavaScript plugins */}
-  {/* jQuery Easing for smooth scrolling between anchors */}
-  {/* Swiper for image and text sliders */}
-  {/* Magnific Popup for lightboxes */}
-  {/* Custom scripts */}
-</>
+    {/* end of content */}
+    {/* Scripts */}
+    {/* jQuery for JavaScript plugins */}
+    {/* jQuery Easing for smooth scrolling between anchors */}
+    {/* Swiper for image and text sliders */}
+    {/* Magnific Popup for lightboxes */}
+    {/* Custom scripts */}
+  </>
 );
 
 export default THIRDONE;
