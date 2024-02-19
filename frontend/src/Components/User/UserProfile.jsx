8b58c6d5b9166feee570869f6108f9899b3f22ec
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-02-20 00:29:31 +0800

    adjustment

--- frontend/src/Components/User/UserProfile.jsx
+++ frontend/src/Components/User/UserProfile.jsx
@@ -82,7 +82,7 @@ const Profile = () => (
       </li>
       <li className="mb-1 group">
         <a
-          href=""
+          href="/"
           className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
         >
           <i className="bx bx-envelope mr-3 text-lg" />
