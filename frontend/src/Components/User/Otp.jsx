fefbbdac54c4a167dc13d39c00ea4d60867e91eb
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-03-10 14:41:13 +0800

    otp and receipt

--- frontend/src/Components/User/Otp.jsx
+++ frontend/src/Components/User/Otp.jsx
@@ -4,6 +4,8 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import './Otpas.css';
+
 const Otp = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -44,25 +46,49 @@ const Otp = () => {
     <>
       <div className="container">
         <div className="main_div">
-          <div className="title">Enter OTP</div>
-          <form onSubmit={handleSubmit}>
-            <div className="input_box">
+        <div className="title" style={{ textAlign: "center" }}>
+        One Time Password
+        </div>
+          <form onSubmit={handleSubmit} className="otp-form">
+            <div className="input-box">
               <input
                 type="text"
                 placeholder="Enter OTP"
                 value={enteredOTP}
                 onChange={handleChange}
                 required
+                style={{
+                  width: "300px", // Adjust the width as needed
+                  height: "40px", // Adjust the height as needed
+                  fontSize: "16px", // Adjust the font size as needed
+                  textAlign: "center", 
+                }}
               />
             </div>
-            <div className="input_box button">
-              <input type="submit" value="Verify OTP" />
-            </div>
+            <button
+              style={{
+                backgroundColor: "#F8FFA2",
+                color: "#000",
+                transition: "background-color 0.3s ease",
+                width: "150px", // Adjust the width as needed
+                height: "50px", // Adjust the height as needed
+                fontSize: "18px", // Adjust the font size as needed
+              }}
+              id="login_button"
+              type="submit"
+              className="inline-block rounded-lg bg-black px-5 py-3 text-sm font-bold text-white hover:bg-white hover:text-black"
+              onMouseOver={(e) => (e.target.style.backgroundColor = "#EFB745")}
+              onMouseOut={(e) => (e.target.style.backgroundColor = "#F8FFA2")}
+            >
+              Verify
+            </button>
           </form>
         </div>
       </div>
       <ToastContainer />
     </>
   );
+  
 };
+
 export default Otp;
